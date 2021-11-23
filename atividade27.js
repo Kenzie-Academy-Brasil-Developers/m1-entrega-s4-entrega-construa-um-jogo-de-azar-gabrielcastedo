652f84ee0d86de9c5907cdb887baad0363503e0b
almeidadylan <almeidadylan793@gmail.com>
2021-11-23 16:51:50 -0400

    botao reset js

--- atividade27.js
+++ atividade27.js
@@ -1,5 +1,5 @@
 const elementos = ['Pedra', 'Papel', 'Tesoura']
-const resultadoFinal = ['Você venceu!', 'Você perdeu!', 'Você empatou!Tente novamente!']
+const resultadoFinal = ['Você venceu!', 'Você perdeu!', 'Você empatou! Tente novamente!']
 
 const botao1 = document.getElementById('pedra')
 const botao2 = document.getElementById('papel')
@@ -36,29 +36,55 @@ function validation(){
     return placar
 }
 
+function resetButton(){
+    escolhafinal = [];
+    resultadoPC = [];
+}
+
 botao1.addEventListener('click', (e)=>{
+    placar = []
     escolhafinal.push(elementos[0])
     console.log(escolhafinal)
     escolhaPC()
     console.log(resultadoPC)
     validation()
     console.log(placar)
+    alert(placar)
+    resetButton()
 })
 
 botao2.addEventListener('click', (e)=>{
+    placar = []
     escolhafinal.push(elementos[1])
     console.log(escolhafinal)
     escolhaPC()
     console.log(resultadoPC)
     validation()
     console.log(placar)
+    alert(placar)
+    resetButton()
 })
 
 botao3.addEventListener('click', (e)=>{
+    placar = []
     escolhafinal.push(elementos[2])
     console.log(escolhafinal)
     escolhaPC()
     console.log(resultadoPC)
     validation()
     console.log(placar)
+    alert(placar)
+    resetButton()
 })
+
+//const click = starting.addEventListener('click', function(e));
+//const reset = document.getElementById('reset').reset();
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', (e)=>{
+    escolhafinal = [];
+    resultadoPC = [];
+    placar = []
+})
+
+
