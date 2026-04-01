0ae5062b2052575f0e28e4e202257a3bd83c36eb
almeidadylan <almeidadylan793@gmail.com>
2026-04-01 17:54:58 -0400

    refactor: remove console.log from `save-input-name.js`

--- assets/script/save-input-name.js
+++ assets/script/save-input-name.js
@@ -3,9 +3,7 @@ const button = document.querySelector(".send-name-button");
 
 button.addEventListener("click", () => {
   const username = input.value.trim();
-  console.log("Username:", username);
   sessionStorage.setItem("username", username);
   const storedUsername = sessionStorage.getItem("username");
-  console.log("Stored Username:", storedUsername);
 });
 
